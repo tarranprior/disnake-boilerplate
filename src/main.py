f5581064a882b23265edc693281d5e049ebc5c29
Tarran Prior <Tarranprior@protonmail.com>
2025-04-21 19:27:10 +0100

    🔧 Update /disnake-boilerplate/

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 import disnake
 from templates.bot import Bot
-from src.utils.helpers import load_configuration
+from utils.helpers import configuration
 
 
 load_dotenv()
@@ -14,9 +14,8 @@ if __name__ == "__main__":
 
     bot = Bot(
         activity=disnake.Game(
-            name=load_configuration()["configuration"]["activity"]
+            name=configuration()["configuration"]["activity"]
         ),
-        help_command=None,
         intents=disnake.Intents.all(),
         owner_id=int(env["BOT_OWNER"])
     )
